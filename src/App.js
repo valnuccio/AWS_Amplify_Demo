fc55398fc6c1cefb8c9d5f6b2c4730d128a50269
valnuccio <val.nuccio@gmail.com>
2021-02-08 09:47:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
@@ -17,9 +18,10 @@ function App() {
         >
           Learn React
         </a>
+        <div><AmplifySignOut/></div>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
